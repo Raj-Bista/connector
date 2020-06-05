5dc931097f6ba361882e257d0eb7b766b6c271e4
rails course from udemy <lomaslove132@gmail.com>
2020-06-05 22:53:36 +1000

    express-validator implemented

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const {check, validationResult } = require('express-validator');
 
 
 //@route POST api/users
 //@des Register user
 //@access public
 
-router.post('/', (req, res)=> {
-console.log(req.body);
+router.post('/', [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Please include valid email adress').isEmail(),
+    check('password', 'Please enter 6 or more character').isLength({min: 6})
+],(req, res)=> {
+ const errors = validationResult(req);
+ if(!errors.isEmpty()) {
+     return res.status(400).json({errors: errors.array()})
+ }
 res.send('User route')});
 
 module.exports = router;
