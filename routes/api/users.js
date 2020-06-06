2e30da0baa9e1d88da1bafa66718f4c42638b7ff
rails course from udemy <lomaslove132@gmail.com>
2020-06-06 11:02:44 +1000

    jsonwebtoken added for users route

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
-const {check, validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
+ 
 
 const User = require('../../models/User');
 //@route POST api/users
@@ -25,7 +28,7 @@ router.post('/', [
 let user = await User.findOne({email});
 
 if(user) {
-    res.status(400).json({errors:[{msg: 'User already exists'}] });
+ res.status(400).json({errors:[{msg: 'User already exists'}] });
 }
 
  // get user gravatar
@@ -50,16 +53,27 @@ await user.save();
 
  // return jsonwebtoken
 
-res.send('User registered')
+ const payload = {
+    user: {
+      id: user.id
+    }
+  };
 
+  jwt.sign(
+    payload,
+    config.get('jwtSecret'),
+    { expiresIn: '5 days' },
+    (err, token) => {
+      if (err) throw err;
+      res.json({ token });
+    }
+  );
 } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Server error');
-     
- } 
-     
- }
-)
+  console.error(err.message);
+  res.status(500).send('Server error');
+}
+}
+);
 
  
 module.exports = router;
