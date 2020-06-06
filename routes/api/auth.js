7b52cce372cd49f931eab36c402cce4ba4b9f3bd
rails course from udemy <lomaslove132@gmail.com>
2020-06-06 20:19:34 +1000

    authentication done

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 const auth = require('../../middleware/auth');
-
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
+ 
 
 const User = require('../../models/User')
 
@@ -18,5 +22,60 @@ try {
     res.status(500).send('server error')
 }
 });
+
+//@route POST api/auth
+//@des Authenticate user & 
+//@access public
+
+router.post('/', [
+    
+    check('email', 'Please include valid email adress').isEmail(),
+    check('password', 'Password is required').exists()
+],async (req, res)=> {
+ const errors = validationResult(req);
+ if(!errors.isEmpty()) {
+     return res.status(400).json({errors: errors.array()})
+ }
+ const {email, password}= req.body;
+
+ try {
+// see if user exists
+let user = await User.findOne({email});
+
+if(!user) {
+return res.status(400)
+ .json({errors:[{msg: 'Invalid Credentials'}] });
+}
+
+const isMatch = await bcrypt.compare(password, user.password);
+
+if(!isMatch) {
+return res.status(400)
+ .json({errors:[{msg: 'Invalid Credentials'}] });
+}
+
+ // return jsonwebtoken
+
+ const payload = {
+    user: {
+      id: user.id
+    }
+  };
+
+  jwt.sign(
+    payload,
+    config.get('jwtSecret'),
+    { expiresIn: '5 days' },
+    (err, token) => {
+      if (err) throw err;
+      res.json({ token });
+    }
+  );
+} catch (err) {
+  console.error(err.message);
+  res.status(500).send('Server error');
+}
+}
+);
 
 module.exports = router;
