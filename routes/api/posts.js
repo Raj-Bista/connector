f939f921f0aa7bb22e67e287eb5468e4f8edb3f9
rails course from udemy <lomaslove132@gmail.com>
2020-06-10 09:18:08 +1000

    post route create post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -3,12 +3,44 @@ const router = express.Router();
 const { check, validationResult} = require('express-validator');
 const auth = require('../../middleware/auth');
 
-
+const Post = require('../../models/Post');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 //@route POST api/post
 //@des Create post
 //@access Private
 
-router.get('/', (req, res)=>{
-res.send('user route')});
+router.get('/', [auth, [
+    check('text', 'Text is required').not().isEmpty(),
+
+]
+],
+async(req, res)=>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty) {
+        return res.status(400).json({errors: errors.array() });
+    }
+
+    try {
+        const user = await (await User.findById(req.user.id)).isSelected('-password');
+
+    const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id
+    })
+
+    const post = await newPost.save();
+
+    res.json(post);
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error')
+    }
+}
+)
+ 
 
 module.exports = router;
