5aefa21768168429c96c981aa374f5b5e170d4b9
rails course from udemy <lomaslove132@gmail.com>
2020-06-24 21:16:47 +1000

    post route like unlike route and comment route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -6,11 +6,12 @@ const auth = require('../../middleware/auth');
 const Post = require('../../models/Post');
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
+
 //@route POST api/post
 //@des Create post
 //@access Private
 
-router.get('/', [auth, [
+router.post('/', [auth, [
     check('text', 'Text is required').not().isEmpty(),
 
 ]
@@ -22,7 +23,7 @@ async(req, res)=>{
     }
 
     try {
-        const user = await (await User.findById(req.user.id)).isSelected('-password');
+        const user = await User.findById(req.user.id).isSelected('-password');
 
     const newPost = new Post({
         text: req.body.text,
@@ -40,7 +41,211 @@ async(req, res)=>{
         res.status(500).send('Server Error')
     }
 }
-)
+);
+
+//@route GET api/posts
+//@des Get posts
+//@access Private
+
+router.get('/', auth, async (req, res)=> {
+    try {
+        const posts = await Post.find().sort({date: -1});
+        res.json(posts);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error')
+    }
+})
+
+//@route GET api/post/:id
+//@des Get post by id
+//@access Private
+router.get('/:id', auth, async (req, res)=> {
+    try {
+        const post = await Post.findById(req.params.id);
+        if(!post) {
+            return res.status(404).json({msg: 'Post not found'});
+        }
+
+        res.json(post);
+    } catch (err) {
+        console.error(err.message);
+        if(err.kind === 'ObjectId') {
+            return res.status(404).json({msg: 'Post not found'});
+        }
+        res.status(500).send('Server Error')
+    }
+})
  
+//@route Delete api/post/:id
+//@des delete a post
+//@access Private
+
+router.delete('/:id', auth, async (req, res)=> {
+    try {
+        const posts = await Post.findById(req.params.id);
+
+        if(!post) {
+            return res.status(404).json({msg: 'Post not found'});
+        }
+        // check user
+        if(this.post.user.toString() !== req.user.id) {
+            return res.status(401).json({msg: 'User not authorized'});
+        }
+
+        await post.remove();
+
+        res.json({msg: 'Post removed'});
+    } catch (err) {
+        console.error(err.message);
+        if(err.kind === 'ObjectId') {
+            return res.status(404).json({msg: 'Post not found'});
+        }
+        res.status(500).send('Server Error')
+    }
+})
+
+//@route Put api/posts/likes:id
+//@des Like a post
+//@access Private
+
+router.put('/like/:id', auth, async(req, res)=> {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        // check if the user has already been liked
+        if(post.likes.filter(like=>
+            like.user.toString()=== req.user.id).length
+            > 0 ) {
+                return res.status(400).json({msg: 'Post already liked'});
+            }
+            post.likes.unshift({user: req.user.id});
+            await post.save();
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server not found')
+    }
+
+    
+})
+
+//@route Put api/posts/unlikes:id
+//@des Like a post
+//@access Private
+
+router.put('/like/:id', auth, async(req, res)=> {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        // check if the user has already been liked
+        if(post.likes.filter(like=>
+            like.user.toString()=== req.user.id).length
+            === 0 ) {
+                return res.status(400).json({msg: 'Post has not been liked'});
+            }
+
+        // Get remove index
+
+        const removeIndex = post.likes.map(
+            like=> like.user.toString()).indexOf(
+                req.user.id);
+
+            post.likes.splice(removeIndex, 1);
+
+            await post.save();
+            res.json(post.lokes);
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server not found')
+    }
+
+})
+
+//@route POST api/posts/comment/:id
+//@des Comment on a  post
+//@access Private
+
+router.post('/comment/:id', 
+[auth, [
+    check('text', 'Text is required').not().isEmpty(),
+
+]
+],
+async(req, res)=>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty) {
+        return res.status(400).json({errors: errors.array() });
+    }
+
+    try {
+        const user = await User.findById(req.user.id).isSelected('-password');
+        const post = await Post.findById(req.params.id);
+
+    const newComment = new {
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id
+    };
+
+    post.comments.unshift(newComment);
+
+    await post.save();
+
+    res.json(post.comments);
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error')
+    }
+}
+);
+
+//@route Delete api/posts/comment/:id/:comment_id
+//@des delete a comment
+//@access Private
+
+router.delete('/comment/:id/:comment_id', auth,
+async (req, res)=> {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        //Pull out comment
+
+        const comment = post.comments.find(
+            comment=> comment.id === req.params.comment_id
+        );
+
+        // Make sure comment exists
+
+        if(!comment) {
+            return res.status(404).json({msg: 'Comment does not exist'});
+        }
+
+        // check user
+
+        if(comment.user.toString() !== req.user.id) {
+            return res.status(401).json({msg: 'Not authorised'});
+        }
+
+        // Get remove index
+
+        const removeIndex = post.comments.map(
+            comment=> comment.user.toString()
+        ).indexOf(req.user.id);
+
+        post.comments.splice(removeIndex, 1)
+        await post.save();
+
+        res.json(post.comments);
+
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error')
+    }
+})
 
 module.exports = router;
